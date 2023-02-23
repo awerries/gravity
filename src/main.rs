418c200f8895165a0d8c92a3b70205cd4807beaa
Adam Werries <werries05@gmail.com>
2023-02-23 00:16:24 -0800

    swap to 64 bit math and reorganize tree data structures

--- src/main.rs
+++ src/main.rs
@@ -6,45 +6,47 @@ use bevy::{
     window::PresentMode,
     time::FixedTimestep
 };
+use glam::DVec2;
 use random_color;
 use random_color::RandomColor;
 
 const WINDOW_SIZE: f32 = 1200.;
+const OUT_OF_BOUNDS: f64 = 2.0; // track particles this far out of bounds
 
 // http://arborjs.org/docs/barnes-hut
-const THETA_THRESHOLD: f32 = 0.8;
+const THETA_THRESHOLD: f64 = 0.6;
 
-const GRAVITY: f32 = 6.6743e-11; // m^3 / (kg s^2)
-const DSCALE: f32 = 1_000.; // distance scaling w.r.t. meters
-const SIM_STEP: f32 = 1.0; // time of each sim step, in seconds
+const GRAVITY: f64 = 6.6743e-11; // m^3 / (kg s^2)
+const DSCALE: f64 = 1_000_000.; // distance scaling w.r.t. meters
+const SIM_STEP: f64 = 100.0; // time of each sim step, in seconds
 
-const FPS: f32 = 60.0;
-const TIME_STEP: f32 = 1.0/FPS; // how often bevy will attempt to run the sim, in seconds
+const FPS: f64 = 30.0;
+const TIME_STEP: f64 = 1.0/FPS; // how often bevy will attempt to run the sim, in seconds
 
 const NUM_PARTICLES: u32 = 10000;
-const AVG_PARTICLE_MASS: f32 = 1e15;
-const PARTICLE_MAG_VARIATION: f32 = 1.1;
+const AVG_PARTICLE_MASS: f64 = 1e19;
+const PARTICLE_MAG_VARIATION: f64 = 1.1;
 
-const VEL_VARIATION: f32 = 0.2;
-const GALAXY_WIDTH_SCALE: f32 = 0.2;
-const GALAXY_HEIGHT_SCALE: f32 = 1.15;
+const VEL_VARIATION: f64 = 0.01;
+const GALAXY_WIDTH_SCALE: f64 = 0.2;
+const GALAXY_HEIGHT_SCALE: f64 = 2.0;
 
 // Minimum radius to guard against gravity singularity
-const MIN_R: f32 = 0.05 * DSCALE;
-const MIN_R2: f32 = MIN_R*MIN_R;
+const MIN_R: f64 = 0.01 * DSCALE;
+const MIN_R2: f64 = MIN_R*MIN_R;
 
 // Min grid size to protect against floating point division errors
-const MIN_QUADRANT_LENGTH: f32 = 0.02 * DSCALE;
+const MIN_QUADRANT_LENGTH: f64 = 0.00001 * DSCALE;
 
 
 #[derive(Component)]
 struct Pose {
-    r: Vec2,
-    v: Vec2
+    r: DVec2,
+    v: DVec2
 }
 
 #[derive(Component)]
-struct Mass(f32);
+struct Mass(f64);
 
 
 fn main() {
@@ -79,15 +81,15 @@ fn setup(
     let window = windows.get_primary().unwrap();
     commands.spawn(Camera2dBundle::default());
 
-    let w = window.width() * DSCALE;
+    let w = window.width() as f64 * DSCALE;
     spawn_galaxy(
         &mut commands,
         &mut meshes,
         &mut materials,
         NUM_PARTICLES / 2,
         w * GALAXY_WIDTH_SCALE,
-        Vec2::new(w / 10.0, w / 10.0),
-        Vec2::new(0., -DSCALE / 100.0)
+        DVec2::new(w / 10.0, w / 10.0),
+        DVec2::new(0., -DSCALE / 10000.0)
     );
     spawn_galaxy(
         &mut commands,
@@ -95,8 +97,8 @@ fn setup(
         &mut materials,
         NUM_PARTICLES / 2,
         w * GALAXY_WIDTH_SCALE,
-        Vec2::new(-w / 10.0, -w / 10.0),
-        Vec2::new(0., DSCALE / 100.0)
+        DVec2::new(-w / 10.0, -w / 10.0),
+        DVec2::new(0., DSCALE / 10000.0)
     );
 }
 
@@ -105,11 +107,11 @@ fn spawn_galaxy(
     mut meshes: &mut ResMut<Assets<Mesh>>,
     mut materials: &mut ResMut<Assets<ColorMaterial>>,
     num_particles: u32,
-    diameter: f32,
-    gpos: Vec2,
-    gvel: Vec2
+    diameter: f64,
+    gpos: DVec2,
+    gvel: DVec2
 ) {
-    let gmass = (num_particles as f32) * AVG_PARTICLE_MASS;
+    let gmass = (num_particles as f64) * AVG_PARTICLE_MASS;
     for _ in 0..num_particles {
         let (r, theta, pos) = random_circle_pos(diameter);
         let vel = random_orbital_circle_vel(r, theta, diameter*GALAXY_HEIGHT_SCALE, gmass);
@@ -117,21 +119,21 @@ fn spawn_galaxy(
     }
 }
 
-fn random_circle_pos(diameter: f32) -> (f32, f32, Vec2)
+fn random_circle_pos(diameter: f64) -> (f64, f64, DVec2)
 {
     let mut rng = rand::thread_rng();
     let dist = Uniform::new(0., 1.);
     let r = diameter / 2.0 * rng.sample(dist);
     let theta = rng.sample(dist) * 2. * 3.1415927;
-    (r, theta, Vec2::new(r*theta.cos(), r*theta.sin()))
+    (r, theta, DVec2::new(r*theta.cos(), r*theta.sin()))
 }
 
-fn random_orbital_circle_vel(r: f32, theta: f32, scale_height: f32, total_mass: f32) -> Vec2
+fn random_orbital_circle_vel(r: f64, theta: f64, scale_height: f64, total_mass: f64) -> DVec2
 {
     let mut rng = rand::thread_rng();
     let grav = GRAVITY * total_mass / (r * r + scale_height * scale_height).sqrt();
     let v = (2.0 * grav).sqrt();
-    Vec2::new(
+    DVec2::new(
         v*theta.sin() + v * rng.gen_range(-VEL_VARIATION..VEL_VARIATION),
         -v*theta.cos() + v * rng.gen_range(-VEL_VARIATION..VEL_VARIATION)
     )
@@ -141,8 +143,8 @@ fn spawn_particle(
     commands: &mut Commands,
     meshes: &mut ResMut<Assets<Mesh>>,
     materials: &mut ResMut<Assets<ColorMaterial>>,
-    pos: Vec2,
-    vel: Vec2,
+    pos: DVec2,
+    vel: DVec2,
 ) {
     let color = RandomColor::new().hue(random_color::Color::Blue).to_rgb_array();
     let mut rng = rand::thread_rng();
@@ -151,7 +153,7 @@ fn spawn_particle(
         MaterialMesh2dBundle {
             mesh: meshes.add(shape::Circle::new(1.0).into()).into(),
             material: materials.add(ColorMaterial::from(Color::rgb_u8(color[0], color[1], color[2]))),
-            transform: Transform::from_translation(pos.extend(0.) / DSCALE),
+            transform: Transform::from_translation(Vec3::new(pos.x as f32, pos.y as f32, 0.) / DSCALE as f32),
             ..default()
         },
         Pose {r: pos, v: vel},
@@ -167,16 +169,16 @@ fn update(
     let window = windows.get_primary().unwrap();
 
     // each cycle, we build the quad-tree out of the particles
-    let mut tree = BHTree::new(Quadrant::new(window.width() * DSCALE));
+    let mut tree = BHTree::new(Quadrant::new(OUT_OF_BOUNDS * window.width() as f64 * DSCALE));
     for (_, particle, pose, mass) in &particle_query {
-        tree.insert(Body::new(mass, pose.r, particle))
+        tree.insert(particle, Body::new(mass, pose.r))
     }
 
     // update the position of each particle
-    particle_query.par_for_each_mut(128, |(mut transform, particle, mut pose, mass)| {
+    particle_query.par_for_each_mut(512, |(mut transform, particle, mut pose, mass)| {
         let Mass(mass_f) = *mass;
         // TODO avoid recreating Body in each loop
-        let accel = tree.get_force(&Body::new(mass, pose.r, particle)) / mass_f;
+        let accel = tree.get_force(&particle, &Body::new(mass, pose.r)) / mass_f;
         let t = SIM_STEP;
         let v_i = pose.v;
         let dv = accel * t;
@@ -184,11 +186,13 @@ fn update(
         pose.v += dv;
 
         // set actual rendering position
-        transform.translation.x = pose.r.x / DSCALE;
-        transform.translation.y = pose.r.y / DSCALE;
+        transform.translation.x = (pose.r.x / DSCALE) as f32;
+        transform.translation.y = (pose.r.y / DSCALE) as f32;
 
         // despawn particles that go out of bounds (The other option is to crash! :P or simulate far outside of bounds so they can come back)
-        if (transform.translation.x.abs() >= window.width() / 2.0 - 1.0) || (transform.translation.y.abs() >= window.width() / 2.0 - 1.0) {
+        if (transform.translation.x.abs() >= window.width() / 2.0 * OUT_OF_BOUNDS as f32 - 1.0)
+        || (transform.translation.y.abs() >= window.width() / 2.0 * OUT_OF_BOUNDS as f32 - 1.0)
+        {
             par_commands.command_scope(|mut commands| {
                 commands.entity(particle).despawn();
             });
@@ -201,19 +205,19 @@ enum Corner {NW, NE, SW, SE}
 
 #[derive(Default, Debug)]
 struct Quadrant {
-    center: Vec2,
-    len: f32
+    center: DVec2,
+    len: f64
 }
 
 impl Quadrant {
-    fn new(length: f32) -> Self {
+    fn new(length: f64) -> Self {
         Quadrant {
             len: length,
-            center: Vec2::new(0., 0.)
+            center: DVec2::new(0., 0.)
         }
     }
     /// return true if this Quadrant contains (x,y)
-    fn contains(&self, x: f32, y: f32) -> bool {
+    fn contains(&self, x: f64, y: f64) -> bool {
         let hl = self.len / 2.0;
         (x >= self.center.x - hl) && (x < self.center.x + hl) && (y >= self.center.y - hl) && (y < self.center.y + hl)
     }
@@ -222,93 +226,94 @@ impl Quadrant {
         let hl = self.len / 2.0;
         let ql = hl / 2.0;
         match corner {
-            Corner::NW => Quadrant {center: Vec2::new(self.center.x - ql, self.center.y + ql), len: hl},
-            Corner::NE => Quadrant {center: Vec2::new(self.center.x + ql, self.center.y + ql), len: hl},
-            Corner::SW => Quadrant {center: Vec2::new(self.center.x - ql, self.center.y - ql), len: hl},
-            Corner::SE => Quadrant {center: Vec2::new(self.center.x + ql, self.center.y - ql), len: hl}
+            Corner::NW => Quadrant {center: DVec2::new(self.center.x - ql, self.center.y + ql), len: hl},
+            Corner::NE => Quadrant {center: DVec2::new(self.center.x + ql, self.center.y + ql), len: hl},
+            Corner::SW => Quadrant {center: DVec2::new(self.center.x - ql, self.center.y - ql), len: hl},
+            Corner::SE => Quadrant {center: DVec2::new(self.center.x + ql, self.center.y - ql), len: hl}
         }
     }
 }
 
 
+enum NodeItem {
+    Internal(SubQuadrants),
+    Leaf(Entity)
+}
+
+struct Node {
+    body: Body,
+    item: NodeItem
+}
+
+impl Node {
+    fn new(body: Body, item: NodeItem) -> Self {
+        Node {body, item}
+    }
+}
+
 #[derive(Default)]
 struct BHTree
 {
-    // if there is no body, the BHTree is empty
-    // if the body has no particle, this BHTree will have a subtree.
-    // TODO: make this not be such an asinine struct relationship
-    body: Option<Body>,
     quad: Quadrant,
-    subtree: Option<SubQuadrants>
+    node: Option<Node>
 }
 
 impl BHTree
 {
     fn new(quad: Quadrant) -> Self {
-        // is it weird to impl new just for this?
-        BHTree {
-            quad,
-            ..default()
-        }
+        BHTree {quad, ..default()}
     }
 
-    fn insert(&mut self, b: Body) {
-        if let Some(current_body) = &mut self.body {
-            // We have a body, but does it have an particle or is it a group?
-            if current_body.particle.is_some() {
-                if self.quad.len > MIN_QUADRANT_LENGTH {
-                    // this body is a particle, and we only have 1 particle per region if over the min length, so we split and generate subtrees
-                    self.subtree = Some(SubQuadrants::new(&self.quad));
-                    // copy all the fields, as we're moving the particle down a layer
-                    let copy = current_body.clone();
-                    // clear the actual particle, as this is now an internal node
-                    current_body.particle = None;
-                    // add the new particle to the now-internal node mass and COM
-                    current_body.add(&b);
-
-                    // add both particles as external nodes
-                    let subtree = self.subtree.as_mut().unwrap();
-                    subtree.insert_to_quadrant(copy);
-                    subtree.insert_to_quadrant(b);
-                } else {
-                    // we've already got too small of a grid, just add the mass for a cheap estimate
-                    current_body.add(&b);
+    fn insert(&mut self, particle: Entity, body: Body) {
+        if let Some(current_node) = &mut self.node {
+            match &mut current_node.item {
+                NodeItem::Internal(subquad) => {
+                    current_node.body.add(&body);
+                    subquad.insert_to_quadrant(particle, body);
+                },
+                NodeItem::Leaf(node_particle) => {
+                    if self.quad.len > MIN_QUADRANT_LENGTH {
+                        // we only have 1 particle per region, so we split and generate subtrees
+                        let mut subquad = SubQuadrants::new(&self.quad);
+                        subquad.insert_to_quadrant(node_particle.clone(), current_node.body);
+                        subquad.insert_to_quadrant(particle, body);
+                        current_node.item = NodeItem::Internal(subquad);
+                    }
+                    // implied else: if we've already got too small of a grid, we still add the mass for a cheap estimate
+                    current_node.body.add(&body);
                 }
-            } else {
-                current_body.add(&b);
-                self.subtree.as_mut().unwrap().insert_to_quadrant(b);
             }
         } else {
             // current node has no body, add it here as particle/external
-            self.body = Some(b);
+            self.node = Some(Node::new(body, NodeItem::Leaf(particle)));
         }
     }
 
-
-
-    fn get_force(&self, b: &Body) -> Vec2 {     
-        if let Some(node_body) = &self.body {
-            if let Some(node_particle) = &node_body.particle {
-                if node_particle.index() != b.particle.unwrap().index() {
-                    b.force(node_body)
-                } else {
-                    // index was the same, this is the same particle
-                    Vec2::new(0., 0.)
-                }
-            } else {
-                // current node does not have a particle, but does have a body, and therefore must be an internal node
-                let dist = node_body.pos.distance(b.pos);
-                if self.quad.len / dist < THETA_THRESHOLD {
-                    // treat node as a single body
-                    b.force(node_body)
-                } else {
-                    // traverse the tree, returning the total force
-                    self.subtree.as_ref().unwrap().get_force(b)
+    fn get_force(&self, p: &Entity, b: &Body) -> DVec2 {
+        if let Some(current_node) = &self.node {
+            match &current_node.item {
+                NodeItem::Internal(subquad) => {
+                    let dist = current_node.body.pos.distance(b.pos);
+                    if self.quad.len / dist < THETA_THRESHOLD {
+                        // treat node as a single body
+                        b.force(&current_node.body)
+                    } else {
+                        // traverse the tree, returning the total force
+                        subquad.get_force(p, b)
+                    }
+                },
+                NodeItem::Leaf(node_particle) => {
+                    if node_particle.index() != p.index() {
+                        b.force(&current_node.body)
+                    } else {
+                        // index was the same, this is the same particle
+                        DVec2::new(0., 0.)
+                    }
                 }
             }
         } else {
             // there's no body at self, so there's no force
-            Vec2::new(0., 0.)
+            DVec2::new(0., 0.)
         }
     }
 }
@@ -331,40 +336,33 @@ impl SubQuadrants {
         }
     }
 
-    fn get_force(&self, b: &Body) -> Vec2 {
-        self.nw.get_force(b) + self.ne.get_force(b) + self.sw.get_force(b) + self.se.get_force(b)
+    fn get_force(&self, p: &Entity, b: &Body) -> DVec2 {
+        self.nw.get_force(p, b) + self.ne.get_force(p, b) + self.sw.get_force(p, b) + self.se.get_force(p, b)
     }
 
-    fn insert_to_quadrant(&mut self, b: Body) {
+    fn insert_to_quadrant(&mut self, p: Entity, b: Body) {
         // this is an internal node, we must have a subtree
         match b {
-            b if b.within(&self.nw.quad) => self.nw.insert(b),
-            b if b.within(&self.ne.quad) => self.ne.insert(b),
-            b if b.within(&self.sw.quad) => self.sw.insert(b),
-            b if b.within(&self.se.quad) => self.se.insert(b),
+            b if b.within(&self.nw.quad) => self.nw.insert(p, b),
+            b if b.within(&self.ne.quad) => self.ne.insert(p, b),
+            b if b.within(&self.sw.quad) => self.sw.insert(p, b),
+            b if b.within(&self.se.quad) => self.se.insert(p, b),
             b => panic!("position {}, {} was not in any quadrant?\n {:#?}, {:#?}, {:#?}, {:#?}", b.pos.x, b.pos.y, self.nw.quad, self.ne.quad, self.sw.quad, self.se.quad)
         }
     }
 }
 
 
-#[derive(Default, Clone)]
+#[derive(Default, Clone, Copy)]
 struct Body {
-    mass: f32,
-    pos: Vec2,
-    // if body has a particle entity, it's an external particle.
-    // if not, it's a collection of particles.
-    particle: Option<Entity>
+    mass: f64,
+    pos: DVec2,
 }
 
 impl Body {
-    fn new(mass: &Mass, pos: Vec2, particle: Entity) -> Self {
-        let Mass(mass) = mass;
-        Body {
-            mass: *mass,
-            pos,
-            particle: Some(particle)
-        }
+    fn new(mass: &Mass, pos: DVec2) -> Self {
+        let Mass(mass) = *mass;
+        Body {mass, pos}
     }
 
     fn within(&self, q: &Quadrant) -> bool {
@@ -375,18 +373,18 @@ impl Body {
         // get total mass, but don't set overall mass until after we update COM
         let total_mass = self.mass + b.mass;
         // update center of mass
-        self.pos.x = (self.pos.x*self.mass + b.pos.x*b.mass) / total_mass;
-        self.pos.y = (self.pos.y*self.mass + b.pos.y*b.mass) / total_mass;
+        self.pos.x = (self.pos.x * self.mass + b.pos.x * b.mass) / total_mass;
+        self.pos.y = (self.pos.y * self.mass + b.pos.y * b.mass) / total_mass;
         self.mass = total_mass;
     }
 
     /// compute force on self from Body
-    fn force(&self, b: &Body) -> Vec2 {
+    fn force(&self, b: &Body) -> DVec2 {
         let dist2 = b.pos.distance_squared(self.pos);
         // protect against tiny floating point values, otherwise we get insane acceleration without collision detection
         // Barnes. "Gravitational softening as a smoothing operation"
         // https://home.ifa.hawaii.edu/users/barnes/research/smoothing/soft.pdf
-        // F_vec = -G * M * (r2-r1) / (|r2-r1|^2 + eps^2)^3/2
+        // F_vec = -G * M1 * M2 * (r2-r1) / (|r2-r1|^2 + eps^2)^3/2
         - GRAVITY * self.mass * b.mass * (self.pos - b.pos) / (dist2 + MIN_R2).powf(3./2.)
     }
 }
