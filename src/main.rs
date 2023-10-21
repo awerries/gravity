f0762333ce63a3c48b7dbeceabf91784d2e3d026
Adam Werries <werries05@gmail.com>
2023-10-21 15:18:10 -0700

    updated to bevy 0.11

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
 use bevy::{
     prelude::*,
-    window::PresentMode,
-    time::FixedTimestep
+    window::{PresentMode, PrimaryWindow},
 };
 
+mod bhtree;
 mod camera;
 mod spawn;
-mod bhtree;
 
 use bhtree::{BHTree, Quadrant};
 
@@ -16,144 +15,136 @@ const OUT_OF_BOUNDS: f32 = 2.0; // track particles this far out of bounds
 // http://arborjs.org/docs/barnes-hut
 const THETA_THRESHOLD: f32 = 1.0;
 
-const GRAVITY: f32 = 1.0; // m^3 / (kg s^2)
+const GRAVITY: f32 = 1e-6; // m^3 / (kg s^2)
 const NEGATIVE_MASS: f32 = 0.; // apply expansion factor
 const DSCALE: f32 = 1.0; // distance scaling w.r.t. pixels. average distance between stars in milky way is 5 light years, or 4.73e16 meters
-const SIM_STEP: f32 = 0.0001; // time of each sim step, in seconds. 1e12 seconds is 31.7k years. takes 230million years for sun to get around milky way.
+const SIM_STEP: f32 = 0.1; // time of each sim step, in seconds. 1e12 seconds is 31.7k years. takes 230million years for sun to get around milky way.
 
-const FPS: f64 = 60.0;
-const TIME_STEP: f64 = 1.0/FPS; // how often bevy will attempt to run the sim, in seconds
+const FPS: f64 = 120.0;
+const TIME_STEP: f64 = 1.0 / FPS; // how often bevy will attempt to run the sim, in seconds
 
-const NUM_PARTICLES: u32 = 10000;
+const NUM_PARTICLES: u32 = 5000;
 const PARTICLE_MASS_LOWER: f32 = 1.0;
 const PARTICLE_MASS_UPPER: f32 = 10.;
 
 const VEL_VARIATION: f32 = 0.01;
 const GALAXY_WIDTH_SCALE: f32 = 0.3;
-const GALAXY_SCALE_FACTOR: f32 = 0.0;
 
 const SPAWN_BLACKHOLES: bool = true;
-const BLACK_HOLE_REL_MASS: f32 = 1e3;
+const BLACK_HOLE_REL_MASS: f32 = 1e4;
 
 // Minimum radius to guard against gravity singularity
-const MIN_R: f32 = 1.0 * DSCALE;
-const MIN_R2: f32 = MIN_R*MIN_R;
+const MIN_R: f32 = 0.5 * DSCALE;
+const MIN_R2: f32 = MIN_R * MIN_R;
 
 // Min grid size to protect against floating point division errors
-const MIN_QUADRANT_LENGTH: f32 = 0.005;
-
+const MIN_QUADRANT_LENGTH: f32 = 5e-3;
 
 #[derive(Component)]
 struct Pose {
     r: Vec2,
     v: Vec2,
-    prev_accel: Option<Vec2>
+    prev_accel: Option<Vec2>,
 }
 
-#[derive(Component)]
+#[derive(Component, Deref)]
 struct Mass(f32);
 
-#[derive(Debug, Clone, Eq, PartialEq, Hash)]
+#[derive(Debug, Clone, Default, Copy, Eq, PartialEq, Hash, States)]
 enum AppState {
-    Running,
+    #[default]
     Paused,
+    Running,
 }
 
 fn main() {
     App::new()
         .insert_resource(ClearColor(Color::rgb(0., 0., 0.)))
         .add_plugins(DefaultPlugins.set(WindowPlugin {
-            window: WindowDescriptor {
-                title: "gravity_sim".to_string(),
-                width: WINDOW_SIZE,
-                height: WINDOW_SIZE,
-                present_mode: PresentMode::AutoVsync,
+            primary_window: Some(Window {
+                title: "gravity".to_string(),
+                resolution: (WINDOW_SIZE, WINDOW_SIZE).into(),
+                present_mode: PresentMode::Immediate,
                 ..default()
-            },
+            }),
             ..default()
         }))
-        .add_state(AppState::Paused)
-        .add_startup_system(spawn::setup)
-        .add_system_set(
-            SystemSet::new()
-            .with_run_criteria(FixedTimestep::step(TIME_STEP))
-            .with_system(update)
+        .add_state::<AppState>()
+        .add_systems(Startup, spawn::setup)
+        .add_systems(
+            Update,
+            (detect_pause, camera::movement, bevy::window::close_on_esc),
         )
-        //.add_system_set(SystemSet::on_update(AppState::Running).with_system(update))
-        .add_system(detect_pause)
-        .add_system(camera::movement)
-        .add_system(bevy::window::close_on_esc)
+        .add_systems(Update, update.run_if(in_state(AppState::Running)))
         .run();
 }
 
-fn detect_pause(mut app_state: ResMut<State<AppState>>, mut keys: ResMut<Input<KeyCode>>) {
+fn detect_pause(
+    current_state: Res<State<AppState>>,
+    mut new_state: ResMut<NextState<AppState>>,
+    mut keys: ResMut<Input<KeyCode>>,
+) {
     if keys.just_pressed(KeyCode::Space) {
-        let current = app_state.current().clone();
-        app_state
-            .set(match current {
-                AppState::Running => AppState::Paused,
-                AppState::Paused => AppState::Running,
-            })
-            .unwrap();
-        // ^ this can fail if we are already in the target state
-        // or if another state change is already queued;
+        new_state.set(match current_state.get() {
+            AppState::Running => AppState::Paused,
+            AppState::Paused => AppState::Running,
+        });
         keys.reset(KeyCode::Space);
     }
 }
 
-
 fn update(
-    windows: Res<Windows>,
+    windows: Query<&Window, With<PrimaryWindow>>,
     par_commands: ParallelCommands,
-    mut particle_query: Query<(&mut Transform, Entity, &mut Pose, &Mass)>
+    mut particle_query: Query<(Entity, &mut Transform, &mut Pose, &Mass)>,
 ) {
-    let bounds = OUT_OF_BOUNDS * windows.get_primary().unwrap().width();
+    let bounds = OUT_OF_BOUNDS * windows.get_single().unwrap().width();
 
     // each cycle, we build the quad-tree out of the particles
     let mut tree = BHTree::new(Quadrant::new(bounds * DSCALE));
-    for (_, particle, pose, mass) in &particle_query {
+    for (particle, _, pose, mass) in &particle_query {
         tree.insert(particle, Body::new(mass, pose.r))
     }
 
     // update the position of each particle
-    particle_query.par_for_each_mut(1024, |(mut transform, particle, mut pose, mass)| {
-        let Mass(mass_f) = *mass;
-        // TODO avoid recreating Body in each loop
-        let mut accel = tree.get_force(&particle, &Body::new(mass, pose.r)) / mass_f;
-        let t = SIM_STEP;
-        
-        // apply "external mass"
-        if NEGATIVE_MASS > 0. {
-            let rmag = pose.r.length();
-            if rmag > 0. {
-                accel += GRAVITY * NEGATIVE_MASS * pose.r.normalize_or_zero() / rmag;
+    particle_query
+        .par_iter_mut()
+        .for_each_mut(|(particle, mut transform, mut pose, mass)| {
+            let Mass(mass_f) = *mass;
+            // TODO avoid recreating Body in each loop
+            let mut accel = tree.get_force(&particle, &Body::new(mass, pose.r)) / mass_f;
+            let t = SIM_STEP;
+
+            // apply "external mass"
+            if NEGATIVE_MASS > 0. {
+                let rmag = pose.r.length();
+                if rmag > 0. {
+                    accel += GRAVITY * NEGATIVE_MASS * pose.r.normalize_or_zero() / rmag;
+                }
             }
-        }
-        
-        // https://en.wikipedia.org/wiki/Leapfrog_integration
-        let dv = accel * t;
-        if let Some(prev) = pose.prev_accel {
-            pose.v += 0.5 * (prev * SIM_STEP + dv);
-        }
-        pose.r = pose.r + (pose.v + 0.5 * dv) * t;
-        pose.prev_accel = Some(accel);
 
-        // set actual rendering position
-        transform.translation.x = pose.r.x / DSCALE;
-        transform.translation.y = pose.r.y / DSCALE;
+            // https://en.wikipedia.org/wiki/Leapfrog_integration
+            let dv = accel * t;
+            if let Some(prev) = pose.prev_accel {
+                pose.v += 0.5 * (prev * SIM_STEP + dv);
+            }
+            pose.r = pose.r + (pose.v + 0.5 * dv) * t;
+            pose.prev_accel = Some(accel);
 
-        // despawn particles that go out of bounds (The other option is to crash! :P or simulate far outside of bounds so they can come back)
-        if (transform.translation.x.abs() >= bounds / 2.0 - 1.0)
-        || (transform.translation.y.abs() >= bounds / 2.0 - 1.0)
-        {
-            par_commands.command_scope(|mut commands| {
-                commands.entity(particle).despawn();
-            });
-        }
-    });
+            // set actual rendering position
+            transform.translation.x = pose.r.x / DSCALE;
+            transform.translation.y = pose.r.y / DSCALE;
+
+            // despawn particles that go out of bounds (The other option is to crash! :P or simulate far outside of bounds so they can come back)
+            if (transform.translation.x.abs() >= bounds / 2.0 - 1.0)
+                || (transform.translation.y.abs() >= bounds / 2.0 - 1.0)
+            {
+                par_commands.command_scope(|mut commands| {
+                    commands.entity(particle).despawn();
+                });
+            }
+        });
 }
-
-
 
 #[derive(Default, Clone, Copy)]
 pub struct Body {
@@ -164,7 +155,7 @@ pub struct Body {
 impl Body {
     fn new(mass: &Mass, pos: Vec2) -> Self {
         let Mass(mass) = *mass;
-        Body {mass, pos}
+        Body { mass, pos }
     }
 
     fn add(&mut self, b: &Body) {
@@ -183,6 +174,6 @@ impl Body {
         // Barnes. "Gravitational softening as a smoothing operation"
         // https://home.ifa.hawaii.edu/users/barnes/research/smoothing/soft.pdf
         // F_vec = -G * M1 * M2 * (r2-r1) / (|r2-r1|^2 + eps^2)^3/2
-        - GRAVITY * self.mass * b.mass * (self.pos - b.pos) / (dist2 + MIN_R2).powf(3./2.)
+        -GRAVITY * self.mass * b.mass * (self.pos - b.pos) / (dist2 + MIN_R2).powf(3. / 2.)
     }
 }
--- src/spawn.rs
+++ src/spawn.rs
@@ -1,53 +1,51 @@
-use rand::Rng;
-use rand::distributions::Uniform;
 use colorgrad;
+use rand::distributions::Uniform;
+use rand::Rng;
 
-use bevy::{
-    prelude::*,
-    sprite::MaterialMesh2dBundle,
-};
+use bevy::{prelude::*, sprite::MaterialMesh2dBundle, window::PrimaryWindow};
 
 pub fn setup(
-    windows: Res<Windows>,
+    windows: Query<&Window, With<PrimaryWindow>>,
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
-    let window = windows.get_primary().unwrap();
+    let window = windows.get_single().unwrap();
     commands.spawn(Camera2dBundle::default());
 
-    let size_factor = 5;
+    let size_factor = 3;
     let f2 = size_factor * size_factor;
-    
+
     let w = window.width() * crate::DSCALE;
-    let parent_particles = crate::NUM_PARTICLES * (f2 - 1) / f2;
+    let num_children = 1;
+    let parent_particles = crate::NUM_PARTICLES * (f2 - num_children) / f2;
     let child_particles = crate::NUM_PARTICLES / f2;
     spawn_galaxy(
         &mut commands,
         &mut meshes,
         &mut materials,
-        crate::NUM_PARTICLES * (f2 - 1) / f2,
+        parent_particles,
         w * crate::GALAXY_WIDTH_SCALE,
         Vec2::new(0.0, 0.0),
         Vec2::new(0.0, 0.0),
-        1.0
+        1.0,
     );
-    
-    let y_offset = w * crate::DSCALE / 5.0;
+
+    let y1 = w * crate::DSCALE / 3.0;
     let bmass = crate::PARTICLE_MASS_LOWER * parent_particles as f32 * crate::BLACK_HOLE_REL_MASS;
-    let v = (crate::GRAVITY * bmass / (y_offset * y_offset).sqrt()).sqrt();
-    let v = v / 2.0; // skewed velocity to make it elliptical
+    let v = (crate::GRAVITY * bmass / (y1 * y1).sqrt()).sqrt();
+    let v = v / 1.5; // skewed velocity to make it elliptical
     spawn_galaxy(
         &mut commands,
         &mut meshes,
         &mut materials,
         child_particles,
         w * crate::GALAXY_WIDTH_SCALE / size_factor as f32,
-        Vec2::new(0.0, y_offset),
+        Vec2::new(0.0, y1),
         Vec2::new(v, 0.0),
-        1.0
+        1.0,
     );
-    
+
     /*
     spawn_grid(
         &mut commands,
@@ -67,11 +65,11 @@ pub fn spawn_grid(
     mut materials: &mut ResMut<Assets<ColorMaterial>>,
     num_particles: u32,
     width: f32,
-    height: f32
+    height: f32,
 ) {
     let mut rng = rand::thread_rng();
-    let xdist = Uniform::new(-width/2.0, width/2.0);
-    let ydist = Uniform::new(-height/2.0, height/2.0);
+    let xdist = Uniform::new(-width / 2.0, width / 2.0);
+    let ydist = Uniform::new(-height / 2.0, height / 2.0);
     let vdist = Uniform::new(0., crate::VEL_VARIATION);
     let vthetadist = Uniform::new(0.0, 2.0 * std::f32::consts::PI);
     let mdist = Uniform::new(crate::PARTICLE_MASS_LOWER, crate::PARTICLE_MASS_UPPER);
@@ -88,7 +86,8 @@ pub fn spawn_grid(
             Vec2::new(rng.sample(xdist), rng.sample(ydist)),
             Vec2::new(v * vtheta.cos(), v * vtheta.sin()),
             mass,
-            get_star_color(mass));
+            get_star_color(mass),
+        );
     }
 }
 
@@ -101,11 +100,11 @@ fn spawn_galaxy(
     diameter: f32,
     gpos: Vec2,
     gvel: Vec2,
-    rotation: f32
+    rotation: f32,
 ) {
     let bmass = crate::PARTICLE_MASS_LOWER * num_particles as f32 * crate::BLACK_HOLE_REL_MASS;
     let mut gmass = 0.; // previously: (num_particles as f32) * crate::AVG_PARTICLE_MASS;
-    if crate::SPAWN_BLACKHOLES { 
+    if crate::SPAWN_BLACKHOLES {
         gmass += bmass;
     }
 
@@ -113,10 +112,9 @@ fn spawn_galaxy(
     let mdist = Uniform::new(crate::PARTICLE_MASS_LOWER, crate::PARTICLE_MASS_UPPER);
     for ii in 0..num_particles {
         let (r, theta, pos) = random_circle_pos(diameter);
-        let scale_r2 = (diameter * crate::GALAXY_SCALE_FACTOR / 2.0).powi(2);
-        let vel = random_orbital_circle_vel(r, theta, scale_r2, gmass, rotation);
+        let vel = random_orbital_circle_vel(r, theta, gmass, rotation);
         let mass = rng.sample(mdist);
-        
+
         spawn_particle(
             &mut commands,
             &mut meshes,
@@ -125,11 +123,10 @@ fn spawn_galaxy(
             pos + gpos,
             vel + gvel,
             mass,
-            get_star_color(mass)
+            get_star_color(mass),
         );
     }
 
-    // spawn black hole
     if crate::SPAWN_BLACKHOLES {
         spawn_particle(
             &mut commands,
@@ -139,39 +136,38 @@ fn spawn_galaxy(
             gpos,
             gvel,
             bmass,
-            Color::DARK_GRAY
+            Color::DARK_GRAY,
         );
     }
 }
 
-fn get_star_color(mass: f32) -> Color
-{
+fn get_star_color(mass: f32) -> Color {
     let g = colorgrad::rd_yl_bu();
-    let mass_ratio = (mass - crate::PARTICLE_MASS_LOWER) / (crate::PARTICLE_MASS_UPPER - crate::PARTICLE_MASS_LOWER);
+    let mass_ratio = (mass - crate::PARTICLE_MASS_LOWER)
+        / (crate::PARTICLE_MASS_UPPER - crate::PARTICLE_MASS_LOWER);
     let mass_ratio_skewed = mass_ratio.sqrt().sqrt();
     let c = g.at(mass_ratio_skewed as f64).to_rgba8();
     Color::rgba_u8(c[0], c[1], c[2], c[3])
 }
 
-fn random_circle_pos(diameter: f32) -> (f32, f32, Vec2)
-{
+fn random_circle_pos(diameter: f32) -> (f32, f32, Vec2) {
     let mut rng = rand::thread_rng();
     let rdist = Uniform::new(0.00001, 1.0_f32);
     let r = (diameter / 2.0) * rng.sample(rdist).sqrt();
 
     let theta_dist = Uniform::new(0., 2.0 * std::f32::consts::PI);
     let theta = rng.sample(theta_dist) * 2. * std::f32::consts::PI;
-    (r, theta, Vec2::new(r*theta.cos(), r*theta.sin()))
+    (r, theta, Vec2::new(r * theta.cos(), r * theta.sin()))
 }
 
-fn random_orbital_circle_vel(r: f32, theta: f32, scale_r2: f32, mass: f32, rotation: f32) -> Vec2
-{
+fn random_orbital_circle_vel(r: f32, theta: f32, mass: f32, rotation: f32) -> Vec2 {
     let mut rng = rand::thread_rng();
     let r = r + crate::MIN_R;
-    let v = (crate::GRAVITY * mass / (r * r + scale_r2).sqrt()).sqrt();
+    let v = (crate::GRAVITY * mass / (r * r).sqrt()).sqrt();
     Vec2::new(
-        rotation*v*theta.sin() + v * rng.gen_range(-crate::VEL_VARIATION..crate::VEL_VARIATION),
-        rotation*-v*theta.cos() + v * rng.gen_range(-crate::VEL_VARIATION..crate::VEL_VARIATION)
+        rotation * v * theta.sin() + v * rng.gen_range(-crate::VEL_VARIATION..crate::VEL_VARIATION),
+        rotation * -v * theta.cos()
+            + v * rng.gen_range(-crate::VEL_VARIATION..crate::VEL_VARIATION),
     )
 }
 
@@ -183,7 +179,7 @@ fn spawn_particle(
     pos: Vec2,
     vel: Vec2,
     mass: f32,
-    color: Color
+    color: Color,
 ) {
     commands.spawn((
         MaterialMesh2dBundle {
@@ -192,7 +188,11 @@ fn spawn_particle(
             transform: Transform::from_translation(Vec3::new(pos.x, pos.y, index) / crate::DSCALE),
             ..default()
         },
-        crate::Pose {r: pos, v: vel, prev_accel: None},
-        crate::Mass(mass)
+        crate::Pose {
+            r: pos,
+            v: vel,
+            prev_accel: None,
+        },
+        crate::Mass(mass),
     ));
 }
